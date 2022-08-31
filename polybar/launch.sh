cb4f89651e4f0d57cf2f9ffe95f26050be9e82ef
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-08-31 22:54:15 +0200

    Initial commit

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# pkill polybar
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar example -r
