6aef3e2d66723292e84ea83f81cd06a9c3f65853
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-09-27 22:26:42 +0200

    Enable capabilities

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -31,11 +31,11 @@ local function on_attach(client, bufnr)
   require("config.lsp.keymaps").setup(client, bufnr)
 end
 
--- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local opts = {
   on_attach = on_attach,
-	-- capabilities = capabilities,
+	capabilities = capabilities,
   flags = {
     debounce_text_changes = 150,
   },
