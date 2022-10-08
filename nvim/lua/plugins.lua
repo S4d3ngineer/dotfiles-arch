eeeac5eda122af11520ed5804f3a3f833a737121
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-10-08 13:35:01 +0200

    Add bufferline with setup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,230 +1,240 @@
 local M = {}
 
 function M.setup()
-	-- Indicate first time installation
-	local packer_bootstrap = false
+  -- Indicate first time installation
+  local packer_bootstrap = false
 
-	-- packer.nvim configuration
-	local conf = {
+  -- packer.nvim configuration
+  local conf = {
 
-		profile = {
-			enable = true,
-			threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-		},
+    profile = {
+      enable = true,
+      threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
 
-		display = {
-			open_fn = function()
-				return require("packer.util").float { border = "rounded" }
-			end,
-		},
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+    },
 
-	}
+  }
 
-	-- Check if packer.nvim is installed
-	-- Run PackerCompile if there are changes in this file
-	local function packer_init()
-		local fn = vim.fn
-		local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-		if fn.empty(fn.glob(install_path)) > 0 then
-			packer_bootstrap = fn.system {
-				"git",
-				"clone",
-				"--depth",
-				"1",
-				"https://github.com/wbthomason/packer.nvim",
-				install_path,
-			}
-			vim.cmd [[packadd packer.nvim]]
-		end
-		vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
-	end
+  -- Check if packer.nvim is installed
+  -- Run PackerCompile if there are changes in this file
+  local function packer_init()
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+      }
+      vim.cmd [[packadd packer.nvim]]
+    end
+    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+  end
 
-	-- Plugins
-	local function plugins(use)
-		use { "wbthomason/packer.nvim" }
+  -- Plugins
+  local function plugins(use)
+    use { "wbthomason/packer.nvim" }
 
-		-- nvim-tree
-		use {
-			"kyazdani42/nvim-tree.lua",
-			wants = "nvim-web-devicons",
-			cmd = { "NvimTreeToggle", "NvimTreeClose" },
-			config = function()
-				require("config.nvimtree").setup()
-			end,
-		}
+    -- nvim-tree
+    use {
+      "kyazdani42/nvim-tree.lua",
+      wants = "nvim-web-devicons",
+      cmd = { "NvimTreeToggle", "NvimTreeClose" },
+      config = function()
+        require("config.nvimtree").setup()
+      end,
+    }
 
-		-- Telescope
-		use {
-			'nvim-telescope/telescope.nvim', tag = '0.1.0',
-			-- or                            , branch = '0.1.x',
-			requires = {
-				'nvim-lua/plenary.nvim',
-				'nvim-treesitter/nvim-treesitter',
-				'BurntSushi/ripgrep'
-			}
-		}
+    -- Telescope
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      -- or                            , branch = '0.1.x',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+        'BurntSushi/ripgrep'
+      }
+    }
 
-		-- WhichKey
-		use {
-			"folke/which-key.nvim",
-			config = function()
-				require("config.whichkey").setup()
-			end,
-		}
+    -- Buffer line
+    use {
+      "akinsho/nvim-bufferline.lua",
+      event = "BufReadPre",
+      wants = "nvim-web-devicons",
+      config = function()
+        require("config.bufferline").setup()
+      end,
+    }
 
-		use {
-			"tpope/vim-commentary",
-		}
+    -- WhichKey
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("config.whichkey").setup()
+      end,
+    }
 
-		-- Treesitter
-		use {
-			"nvim-treesitter/nvim-treesitter",
-			opt = true,
-			event = "BufRead",
-			run = ":TSUpdate",
-			config = function()
-				require("config.treesitter").setup()
-			end,
-			requires = {
-				{ "nvim-treesitter/nvim-treesitter-textobjects" },
-			},
-		}
+    use {
+      "tpope/vim-commentary",
+    }
 
-		-- git
-		use {
-			"sindrets/diffview.nvim",
-			requires = {
-				"nvim-lua/plenary.nvim",
-			},
-		}
+    -- Treesitter
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      opt = true,
+      event = "BufRead",
+      run = ":TSUpdate",
+      config = function()
+        require("config.treesitter").setup()
+      end,
+      requires = {
+        { "nvim-treesitter/nvim-treesitter-textobjects" },
+      },
+    }
 
-		-- Rainbow brackets
-		use {
-			'p00f/nvim-ts-rainbow',
-			after = "nvim-treesitter",
-			config = function()
-				require("config.treesitter").setup()
-			end,
-		}
+    -- git
+    use {
+      "sindrets/diffview.nvim",
+      requires = {
+        "nvim-lua/plenary.nvim",
+      },
+    }
 
-		-- Auto pairs
-		use {
-			"windwp/nvim-autopairs",
-			-- wants = "nvim-treesitter",
-			module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
-			config = function()
-				require("config.autopairs").setup()
-			end,
-		}
+    -- Rainbow brackets
+    use {
+      'p00f/nvim-ts-rainbow',
+      after = "nvim-treesitter",
+      config = function()
+        require("config.treesitter").setup()
+      end,
+    }
 
-		-- Auto tags
-		use {
-			"windwp/nvim-ts-autotag",
-			wants = "nvim-treesitter",
-			event = "InsertEnter",
-			config = function()
-				-- require("nvim-ts-autotag").setup { enable = true }
-				require("nvim-ts-autotag").setup()
-			end,
-		}
+    -- Auto pairs
+    use {
+      "windwp/nvim-autopairs",
+      -- wants = "nvim-treesitter",
+      module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
+      config = function()
+        require("config.autopairs").setup()
+      end,
+    }
 
-		-- Neoscroll
-		use {
-			'karb94/neoscroll.nvim',
-			config = function()
-				require('config.nscroll').setup()
-			end,
-		}
+    -- Auto tags
+    use {
+      "windwp/nvim-ts-autotag",
+      wants = "nvim-treesitter",
+      event = "InsertEnter",
+      config = function()
+        -- require("nvim-ts-autotag").setup { enable = true }
+        require("nvim-ts-autotag").setup()
+      end,
+    }
 
-		-- LSP
-		use {
-			"neovim/nvim-lspconfig",
-			opt = true,
-			event = "BufRead",
-			wants = { "nvim-lsp-installer", "lsp_signature.nvim", "cmp-nvim-lsp" }, -- for nvim-cmp
-			-- wants = { "nvim-lsp-installer", "lsp_signature.nvim", "coq_nvim" },  -- for coq.nvim
-			config = function()
-				require("config.lsp").setup()
-			end,
-			requires = {
-				"williamboman/nvim-lsp-installer",
-				"ray-x/lsp_signature.nvim",
-			},
-		}
+    -- Neoscroll
+    use {
+      'karb94/neoscroll.nvim',
+      config = function()
+        require('config.nscroll').setup()
+      end,
+    }
 
-		-- coq
-		use {
-			"ms-jpq/coq_nvim",
-			branch = "coq",
-			event = "VimEnter",
-			opt = true,
-			run = ":COQdeps",
-			config = function()
-				require("config.coq").setup()
-			end,
-			requires = {
-				{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-				{ "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
-			},
-			disable = true,
-		}
+    -- LSP
+    use {
+      "neovim/nvim-lspconfig",
+      opt = true,
+      event = "BufRead",
+      wants = { "nvim-lsp-installer", "lsp_signature.nvim", "cmp-nvim-lsp" }, -- for nvim-cmp
+      -- wants = { "nvim-lsp-installer", "lsp_signature.nvim", "coq_nvim" },  -- for coq.nvim
+      config = function()
+        require("config.lsp").setup()
+      end,
+      requires = {
+        "williamboman/nvim-lsp-installer",
+        "ray-x/lsp_signature.nvim",
+      },
+    }
 
-		-- nvim-cmp
-		use {
-			"hrsh7th/nvim-cmp",
-			event = "InsertEnter",
-			opt = true,
-			config = function()
-				require("config.cmp").setup()
-			end,
-			wants = { "LuaSnip" },
-			requires = {
-				"hrsh7th/cmp-buffer",
-				"hrsh7th/cmp-path",
-				"hrsh7th/cmp-nvim-lua",
-				"ray-x/cmp-treesitter",
-				-- "hrsh7th/cmp-cmdline",
-				"saadparwaiz1/cmp_luasnip",
-				"hrsh7th/cmp-nvim-lsp",
-				{
-					"L3MON4D3/LuaSnip",
-					wants = "friendly-snippets",
-					config = function()
-						require("config.luasnip").setup()
-					end,
-				},
-				"rafamadriz/friendly-snippets",
-			},
-			disable = false,
-		}
+    -- coq
+    use {
+      "ms-jpq/coq_nvim",
+      branch = "coq",
+      event = "VimEnter",
+      opt = true,
+      run = ":COQdeps",
+      config = function()
+        require("config.coq").setup()
+      end,
+      requires = {
+        { "ms-jpq/coq.artifacts", branch = "artifacts" },
+        { "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
+      },
+      disable = true,
+    }
 
-		-- Better icons
-		use {
-			"kyazdani42/nvim-web-devicons",
-			module = "nvim-web-devicons",
-			config = function()
-				require("nvim-web-devicons").setup { default = true }
-			end,
-		}
+    -- nvim-cmp
+    use {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      opt = true,
+      config = function()
+        require("config.cmp").setup()
+      end,
+      wants = { "LuaSnip" },
+      requires = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lua",
+        "ray-x/cmp-treesitter",
+        -- "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lsp",
+        {
+          "L3MON4D3/LuaSnip",
+          wants = "friendly-snippets",
+          config = function()
+            require("config.luasnip").setup()
+          end,
+        },
+        "rafamadriz/friendly-snippets",
+      },
+      disable = false,
+    }
 
-		-- Catppuccin
-		use {
-			"catppuccin/nvim",
-			as = "catppuccin",
-			config = function()
-				vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-				require("config.catp").setup()
-				vim.cmd [[colorscheme catppuccin]]
-			end,
-		}
+    -- Better icons
+    use {
+      "kyazdani42/nvim-web-devicons",
+      module = "nvim-web-devicons",
+      config = function()
+        require("nvim-web-devicons").setup { default = true }
+      end,
+    }
 
-	end
+    -- Catppuccin
+    use {
+      "catppuccin/nvim",
+      as = "catppuccin",
+      config = function()
+        vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+        require("config.catp").setup()
+        vim.cmd [[colorscheme catppuccin]]
+      end,
+    }
 
-	packer_init()
+  end
 
-	local packer = require "packer"
-	packer.init(conf)
-	packer.startup(plugins)
+  packer_init()
+
+  local packer = require "packer"
+  packer.init(conf)
+  packer.startup(plugins)
 end
 
 return M
