f056ac3efe1ee090daffd6b7ec707054ab13a6b4
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-10-08 12:03:37 +0200

    Add Telescope keybindings

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -44,3 +44,9 @@ keymap("n", "<leader>n", ":NvimTreeToggle<CR>", default_opts)
 -- Open/close diffview
 keymap("n", "<leader>o", ":DiffviewOpen<CR>", default_opts)
 keymap("n", "<leader>c", ":DiffviewClose<CR>", default_opts)
+
+-- Telescope
+keymap('n', 'ff', "<cmd>Telescope find_files<CR>", default_opts)
+keymap('n', 'fg', "<cmd>Telescope live_grep<CR>", default_opts)
+keymap('n', 'fb', "<cmd>Telescope buffers<CR>", default_opts)
+keymap('n', 'fh', "<cmd>Telescope help_tags<CR>", default_opts)
