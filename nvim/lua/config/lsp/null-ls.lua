c27f62f936518465e19eb498811ff0f7c27d21c1
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-11-28 22:28:03 +0100

    Add eslint

--- nvim/lua/config/lsp/null-ls.lua
+++ nvim/lua/config/lsp/null-ls.lua
@@ -4,10 +4,12 @@ function M.setup(opts)
   local null_ls = require("null-ls")
 
   local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup({
     sources = {
       formatting.prettierd,
+      diagnostics.eslint,
     },
     on_attach = opts.on_attach
   })
