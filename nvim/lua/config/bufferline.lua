e4a7ed1fa9cc26ddd1a126877e6a315e8f391309
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-12-29 22:20:30 +0100

    Change separator style

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -5,10 +5,18 @@ function M.setup()
     options = {
       numbers = "none",
       diagnostics = "nvim_lsp",
-      separator_style = "thin",
+      separator_style = "slant",
       show_tab_indicators = true,
       show_buffer_close_icons = false,
       show_close_icon = false,
+      -- not working !!
+      offsets = {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        text_align = "left",
+        separator = true,
+        padding = 1
+      }
     },
   }
 end
